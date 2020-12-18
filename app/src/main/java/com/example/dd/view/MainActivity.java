9f09aa39fb2c865c3d949627f3a9602c238f7102
xiaoxin-aosi <3042874090@qq.com>
2020-12-18 08:58:31 +0800

    ee

--- app/src/main/java/com/example/dd/view/MainActivity.java
+++ app/src/main/java/com/example/dd/view/MainActivity.java
@@ -8,27 +8,55 @@ import android.os.Bundle;
 
 import com.example.dd.R;
 import com.example.dd.adapter.RelAdapter;
+import com.example.dd.base.BaseActivity;
 import com.example.dd.bean.NewsBean;
+import com.example.dd.contract.RelContract;
+import com.example.dd.presenter.ImpPresenter;
+import com.example.dd.utils.URLConstant;
 
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity<ImpPresenter> implements RelContract.RelView<NewsBean> {
     private RecyclerView rcy;
     private ArrayList<NewsBean.NewsDTO> list;
     private RelAdapter myAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initView();
+    protected void initData() {
+        presenter.getData(URLConstant.NEWSLIST);
     }
 
-    private void initView() {
+    @Override
+    protected void initView() {
         rcy = findViewById(R.id.rcy);
         rcy.setLayoutManager(new LinearLayoutManager(this));
         list = new ArrayList<>();
         myAdapter = new RelAdapter(list, this);
         rcy.setAdapter(myAdapter);
+    }
+
+
+    @Override
+    protected ImpPresenter getPresenter() {
+        return new ImpPresenter(this);
+    }
+
+    @Override
+    protected int getContenrById() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void onSucess(NewsBean newsBean) {
+        List<NewsBean.NewsDTO> news = newsBean.getNews();
+        list.addAll(news);
+        myAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onFail(String error) {
+
     }
 }
